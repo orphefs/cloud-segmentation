983b84ec6b7d0896ffd70624bb80386007c8d667
Orfeas Kypris <kypriso@gmail.com>
2022-03-07 19:01:42 +0200

    improve metrics

--- src/predict.py
+++ src/predict.py
@@ -1,23 +1,30 @@
 import matplotlib.pyplot as plt
+import random
 import os
 from pathlib import Path
 import numpy as np
 import torch
-
+from torch.utils.data import DataLoader
+from typing import List
+from sklearn.metrics import confusion_matrix
 from definitions import DATA_DIR
 from src.model.unet import UNET
 from src.utils.utils2 import get_dataloaders
 
-def accuracy_metric(inp, targ):
-    mask = targ == 1
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
 
-    accuracy = (inp[mask] == targ[mask]).mean()
+
+def accuracy_metric(inp, targ):
+    mask = targ == inp
+    accuracy = mask.mean()
     return accuracy
 
-def plot_results(x, y):
-    out_data = model(x)
 
-    # plotting
+def plot_results(x, y, output, pred_probs, threshold_value):
+    # Plot inputs (R,G,B,NIR)
     fig, ax = plt.subplots(nrows=3, ncols=4)
     ax[0, 0].imshow(x[0, 0, :, :])
     ax[0, 0].set_title("R")
@@ -28,59 +35,73 @@ def plot_results(x, y):
     ax[0, 3].imshow(x[0, 3, :, :])
     ax[0, 3].set_title("NIR")
 
-    model_output = out_data.detach().numpy()[0, 0, :, :]
-    ax[1, 0].imshow(model_output)
-    ax[1, 0].set_title("Model output")
-
+    # Plot true mask and histogram
     true_mask = y.detach().numpy()[0, 0, :, :]
-    ax[1, 1].imshow(true_mask)
-    ax[1, 1].set_title("True Mask")
+    ax[1, 0].imshow(true_mask)
+    ax[1, 0].set_title("True Mask")
+    ax[2, 0].hist(true_mask.flatten(), bins=50)
 
-    super_threshold_indices = model_output > 0.0
-    model_output[super_threshold_indices] = 1
-    model_output[~super_threshold_indices] = 0
+    # Plot raw model output
+    output = output.detach().numpy().squeeze()
+    ax[1, 1].imshow(output)
+    ax[1, 1].set_title("Model output")
+    ax[2, 1].hist(output.flatten(), bins=50)
 
-    ax[1, 2].imshow(model_output)
-    ax[1, 2].set_title("Thresholded model output")
+    # Plot probabilities (sigmoid output)
+    pprobs = pred_probs.detach().numpy().squeeze()
+    ax[1, 2].imshow(pprobs)
+    ax[1, 2].set_title("Probabilities (sigmoid out)")
+    ax[2, 2].hist(pprobs.flatten(), bins=50)
 
-    ax[2, 0].hist(model_output.flatten(), bins=100)
-    ax[2, 1].hist(true_mask.flatten(),  bins=100)
-    ax[2, 2].hist(model_output.flatten(), bins=100)
+    # threshold in order to calculate accuracy metric
+    pprobs[pprobs >= threshold_value] = 1
+    pprobs[pprobs < threshold_value] = 0
 
-    accuracy = accuracy_metric(inp=model_output, targ=true_mask)
+    # Plot thresholded output
+    ax[1, 3].imshow(pprobs)
+    ax[1, 3].set_title("Thresholded at {}".format(threshold_value))
+    ax[2, 3].hist(pprobs.flatten(), bins=50)
+
+    pprobs = 1 - pprobs
+
+    accuracy = accuracy_metric(inp=pprobs, targ=true_mask)
+
+    print(confusion_matrix(true_mask.flatten(), pprobs.flatten()))
+
     fig.suptitle("Accuracy: {}".format(accuracy), fontsize=16)
 
     plt.show()
 
-def predict():
-    # load data
-    train_dl, valid_dl = get_dataloaders(
-        path_to_tiled_img_dir=os.path.join(DATA_DIR, "tiled", "images"),
-        path_to_tiled_label_dir=os.path.join(DATA_DIR, "tiled", "labels"),
-        batch_size=1,
-        split=(1700, 64)
 
-    )
-
-    # prepare model
-    path_to_checkpoints_dir = Path(DATA_DIR) / "checkpoints"
-
-    checkpoint = torch.load(path_to_model_checkpoint)
+def predict(path_to_model_checkpoint: str, inputs: List[torch.Tensor], true_masks: List[torch.Tensor],
+            threshold_value: float):
+    checkpoint = torch.load(path_to_model_checkpoint, map_location=torch.device(device))
     model = UNET(n_channels=4, n_classes=1, bilinear=True)
     model.load_state_dict(checkpoint['model_state_dict'])
-    # loss_function = nn.BCEWithLogitsLoss()
     optimizer = torch.optim.RMSprop(model.parameters(), lr=0.01)
     optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
     epoch = checkpoint['epoch']
     loss = checkpoint['loss']
-
     model.eval()
+    output = model(inputs)
+    pred_probs = torch.sigmoid(output)[0]
+    plot_results(inputs, true_masks, output, pred_probs, threshold_value)
 
-    batches = list(train_dl)
-    batch_no = 319
-    x, y = batches[batch_no]
-    plot_results(x, y)
 
 if __name__ == '__main__':
-    predict(path_to_model_checkpoint)
-    typer.run(predict)
+    # load data
+    _, valid_dl = get_dataloaders(
+        path_to_tiled_img_dir=os.path.join(DATA_DIR, "tiled", "images"),
+        path_to_tiled_label_dir=os.path.join(DATA_DIR, "tiled", "labels"),
+        batch_size=1,
+        split=(3300, 228)
+    )
+
+    batches = list(valid_dl)
+    # no_batch = random.randrange(0,len(batches))
+    no_batch = 81
+    print(no_batch)
+    x, y = batches[no_batch]
+
+    predict(path_to_model_checkpoint="/home/orphefs/Dropbox/job_applications/overstory/model.pt",
+        inputs=x, true_masks=y, threshold_value=0.99)
--- src/utils/utils2.py
+++ src/utils/utils2.py
@@ -299,12 +299,12 @@ def get_dataloaders(path_to_tiled_img_dir: str, path_to_tiled_label_dir: str, ba
 
 
 if __name__ == '__main__':
-    tile_shape = (1024, 1024)
+    tile_shape = (256, 256)
 
     # tile the images
     path_to_raw_images = os.path.join(DATA_DIR, "raw", "images")
     path_to_tiled_images = os.path.join(DATA_DIR, "tiled", "images")
-    paths_to_raw_images = glob.glob(os.path.join(path_to_raw_images, "*.tif"))
+    paths_to_raw_images = sorted(glob.glob(os.path.join(path_to_raw_images, "*.tif")))
     image_tiler = ImageTiler(paths_to_raw_images)
     image_tiler.extract_tile(paths_to_raw_images, path_to_tiled_images, tile_shape=tile_shape,
         bands=["r", "g", "b", "nir"])
@@ -312,7 +312,7 @@ if __name__ == '__main__':
     # tile the labels
     path_to_raw_images = os.path.join(DATA_DIR, "raw", "labels")
     path_to_tiled_images = os.path.join(DATA_DIR, "tiled", "labels")
-    paths_to_raw_images = glob.glob(os.path.join(path_to_raw_images, "*.tif"))
+    paths_to_raw_images = sorted(glob.glob(os.path.join(path_to_raw_images, "*.tif")))
     image_tiler = ImageTiler(paths_to_raw_images)
     image_tiler.extract_tile(paths_to_raw_images, path_to_tiled_images, tile_shape=tile_shape,
         bands=["r", ])
