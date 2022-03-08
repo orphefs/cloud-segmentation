5ca801bf580b53f7d76f5cc2b9841c96d524983a
Orfeas Kypris <kypriso@gmail.com>
2022-03-08 16:58:57 +0200

    sort paths to images/labels for reproducibility when tiling

--- src/utils/tile_dataset.py
+++ src/utils/tile_dataset.py
@@ -10,18 +10,18 @@ def tile_dataset(tile_shape: Tuple[int, int] = (1024, 1024)):
     # tile the images
     path_to_raw_images = os.path.join(DATA_DIR, "raw", "images")
     path_to_tiled_images = os.path.join(DATA_DIR, "tiled", "images")
-    paths_to_raw_images = glob.glob(os.path.join(path_to_raw_images, "*.tif"))
+    paths_to_raw_images = sorted(glob.glob(os.path.join(path_to_raw_images, "*.tif")))
     image_tiler = ImageTiler(paths_to_raw_images)
     image_tiler.extract_tile(paths_to_raw_images, path_to_tiled_images, tile_shape=tile_shape,
                              bands=["r", "g", "b", "nir"])
     # tile the labels
     path_to_raw_images = os.path.join(DATA_DIR, "raw", "labels")
     path_to_tiled_images = os.path.join(DATA_DIR, "tiled", "labels")
-    paths_to_raw_images = glob.glob(os.path.join(path_to_raw_images, "*.tif"))
+    paths_to_raw_images = sorted(glob.glob(os.path.join(path_to_raw_images, "*.tif")))
     image_tiler = ImageTiler(paths_to_raw_images)
     image_tiler.extract_tile(paths_to_raw_images, path_to_tiled_images, tile_shape=tile_shape,
                              bands=["r", ])
 
 
 if __name__ == '__main__':
-    tile_dataset(tile_shape=(256, 256))
+    tile_dataset(tile_shape=(1024, 1024))
